ca5c445dbb048f834cc7dcf824697bb882bd2b8f
Balakumaran99 <91005620+Balakumaran99@users.noreply.github.com>
2021-09-21 23:29:23 +0530

    Add files via upload

---
+++ IfConditionWithoutOpenClosebraces.java
@@ -0,0 +1,10 @@
+class IfConditionWithoutOpenClosebraces{
+public static void main(String[] args){
+int ivalue=2;
+if(ivalue == 3)
+System.out.println(ivalue++);
+else 
+	System.out.println("no");
+
+}
+}
